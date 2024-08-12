e0edeaffe7877692e5bd56c2dbc9250ebc1ffa8a
Artem D. <89469919+Earlpride@users.noreply.github.com>
2024-08-12 14:12:28 +0200

    Create MapperLocator.java

---
+++ main/net/gray/connect/mapper/test/MapperLocator.java
@@ -0,0 +1,21 @@
+package net.gray.connect.mapper.test;
+
+import net.gray.connect.mapper.test.mapstruct.mapper.IBlackMapper;
+import net.gray.connect.mapper.test.mapstruct.mapper.IWhiteMapper;
+
+import jakarta.inject.Inject;
+
+public class MapperLocator {
+	@Inject
+	private IBlackMapper blackMapper;
+	@Inject
+	private IWhiteMapper whiteMapper;
+	
+	public IBlackMapper getBlackMapper() {
+		return blackMapper;
+	}
+	
+	public IWhiteMapper getWhiteMapper() {
+		return whiteMapper;
+	}
+}
